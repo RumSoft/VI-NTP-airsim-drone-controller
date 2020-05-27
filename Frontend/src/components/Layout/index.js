b6857468fbfcd5e9ea895ee347f801f1c5c9124e
Dominik Ruman <d@ruman.dev>
2020-05-28 01:58:35 +0200

    xd

--- Frontend/src/components/Layout/index.js
+++ Frontend/src/components/Layout/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import { Grid } from "@material-ui/core";
 import Map from "../Map";
+import "./index.scss";
 
 export default class Layout extends Component {
   render() {
     return (
       <Grid container>
-        <Grid item xs={8}>
+        <Grid item xs={8} className="map">
           <Map />
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={4} className="sidebar">
           d
         </Grid>
       </Grid>
--- Frontend/src/components/Map/index.js
+++ Frontend/src/components/Map/index.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import img1 from "./mapka.jpg";
 
 export default class Map extends Component {
   render() {
     return (
       <div>
-        <img src={img1} />
+        <p>xdd</p>
       </div>
     );
   }
