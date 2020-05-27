a4f284f79c9b215481814beae4da927656411821
Dominik Ruman <d@ruman.dev>
2020-05-28 01:21:37 +0200

    init front

---
+++ Frontend/src/components/Map/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import img1 from "./mapka.jpg";
+
+export default class Map extends Component {
+  render() {
+    return (
+      <div>
+        <img src={img1} />
+      </div>
+    );
+  }
+}
