80d32435e3b6ba309258e9f6a134803ae80c2919
Dominik Ruman <d@ruman.dev>
2020-05-29 22:38:00 +0200

    x

--- Frontend/src/components/Waypoint/index.js
+++ Frontend/src/components/Waypoint/index.js
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 
 export default class Waypoint extends Component {
   render() {
+    const lat = (this.props.latitude || 0).toFixed(6);
+    const lng = (this.props.longitude || 0).toFixed(6);
+
     return (
       <div>
         <p style={{ color: this.props.color }}>XX</p>
-        {this.props.latitude || 0},{this.props.longitude || 0}
+        {lat}, {lng}
       </div>
     );
   }
