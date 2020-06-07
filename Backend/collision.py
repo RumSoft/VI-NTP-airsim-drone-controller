36a8d4f27ccc10031132d02da449e7d6f47e82ef
Dominik Ruman <d@ruman.dev>
2020-06-07 17:02:33 +0200

    fuzzy logic prototype for collision-avoided movement

--- Backend/collision.py
+++ Backend/collision.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import math
+from fuzzy import Fuzzy
 
 from settings import COLLISION_BUFFER
 
@@ -10,6 +12,9 @@ class Collision:
         self.terrain_collision = False
 
         self.image = None
+        self.wall_image = None
+        self.terrain_image = None
+        self.fuzzy = Fuzzy()
 
     def process_wall_image(self, raw_image):
         self.image = self._process_raw_image(raw_image)
@@ -17,12 +22,20 @@ class Collision:
 
         self._detect_wall_collision()
         # self._show_image()
+        self.wall_image = self.image
+
+        bands_wall = np.hsplit(self.image, 3)
+        maxes_wall = [np.average(x)/255 for x in bands_wall]
+        self.movement = self.fuzzy.calculate(maxes_wall[0], maxes_wall[1], maxes_wall[2])
+        print(self.movement)
+        self._show_image()
 
     def process_terrain_image(self, raw_image):
         self.image = self._process_raw_image(raw_image)
         self._prepare_image()
 
         self._detect_terrain_collision()
+        self.terrain_image = self.image
         # self._show_image()
 
     def _detect_wall_collision(self):
@@ -39,9 +52,19 @@ class Collision:
 
     @staticmethod
     def _process_raw_image(raw_image):
+
         image = np.array(raw_image.image_data_float, dtype=np.float)
         image = image.clip(0, 255)
         image = image.reshape(raw_image.height, raw_image.width)
+        image = np.delete(image, 1, 1)
+
+        kernel = np.ones((5, 5))/25
+        image = cv2.filter2D(image, -1, kernel) * 5
+        image[image >= 80] = 255
+        image[(image >= 50) & (image < 80)] = 125
+
+        image = cv2.erode(image, kernel, iterations=2)
+        image = image.clip(0, 255)
 
         processed_image = np.array(image, dtype=np.uint8)
         return processed_image
@@ -59,5 +82,5 @@ class Collision:
         self.current_bottom = self.maxes_bottom[2]
 
     def _show_image(self):
-        cv2.imshow("Top", self.image)
+        cv2.imshow("Top", self.wall_image)
         cv2.waitKey(1)
